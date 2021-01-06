b6dd4b367ba3bcbb6309fc0789f3d4429acbb451
Jacek Smolak <jacek.smolak@gmail.com>
2021-01-06 17:29:43 +0100

    Configure and run prettier CLI script

--- components/Code/index.js
+++ components/Code/index.js
@@ -2,7 +2,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const Code = ({ children }) => (
-    <SyntaxHighlighter language="javascript" style={a11yDark}>
-        {children}
-    </SyntaxHighlighter>
+  <SyntaxHighlighter language="javascript" style={a11yDark}>
+    {children}
+  </SyntaxHighlighter>
 );
