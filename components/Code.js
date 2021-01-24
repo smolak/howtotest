cbdd18caed4f76fed2a988ff44a9fd72d1878567
Jacek Smolak <jacek.smolak@gmail.com>
2021-01-24 16:21:25 +0100

    Wrap code lines

--- components/Code.js
+++ components/Code.js
@@ -2,7 +2,12 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { okaidia, coy } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const Code = ({ children, inConversation = false, language = 'javascript' }) => (
-  <SyntaxHighlighter language={language} style={inConversation ? coy : okaidia}>
+  <SyntaxHighlighter
+    lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
+    wrapLines
+    language={language}
+    style={inConversation ? coy : okaidia}
+  >
     {children}
   </SyntaxHighlighter>
 );
