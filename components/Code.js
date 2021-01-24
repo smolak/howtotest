fe241f8c2b624e2426164dc0519a7bc4a2f82e76
Jacek Smolak <jacek.smolak@gmail.com>
2021-01-24 15:55:30 +0100

    Remove not used vars

--- components/Code.js
+++ components/Code.js
@@ -1,5 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { okaidia, coy, prism, coldarkDark, coldarkCold } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { okaidia, coy } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const Code = ({ children, inConversation = false, language = 'javascript' }) => (
   <SyntaxHighlighter language={language} style={inConversation ? coy : okaidia}>
